1f8730f546e523374ee3ee8bc68ea48224bb2cad
Carlos Gonzalez <cagl18@gmail.com>
2019-06-16 11:47:51 -0400

    added links and on scroll animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    /* For the sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down"){
             $('nav').addClass('sticky');
@@ -8,7 +9,38 @@ $(document).ready(function(){
         }
     },{ offset: '60px'
     });
-    // $('h1').click(function(){
-    //     $(this).css('background-color', '#ff0000');
-    // });
+  
+    /* Scrool on buttons */
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 100);
+    });
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 100);
+    });
+    
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+
 });
